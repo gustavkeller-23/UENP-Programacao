1dc21ff3276b5d0e2f6ad9529d931f73ba5e4906
Gustav <gustavkeller2708@gmail.com>
2023-09-30 18:42:47 -0300

    Exercício 1 String Concluído e Começando o Exercício 2

--- Programação C/String (Array de caracteres)/Atividade_1.c
+++ Programação C/String (Array de caracteres)/Atividade_1.c
@@ -7,20 +7,17 @@
 
 int main(){
     char frase[30];
-    char frase_invertida[30];
-    int i, tamanho_da_frase, j=0;
+    int i, tamanho_da_frase;
 
     printf("Digite uma frase: \n");
     gets(frase);
 
     tamanho_da_frase = strlen(frase);
 
-    for (i = tamanho_da_frase-1; i > 0; i--){
-        frase_invertida[j] = frase[i];
-        j++;
+    printf("A frase foi %s, e agora e: ", frase);
+    for (i = tamanho_da_frase-1; i >= 0; i--){
+        printf("%c", frase[i]);
     }
-    
-    printf("A frase foi %s, e agora e: %s", frase, frase_invertida);
 
     return 0;
 }
--- Programação C/String (Array de caracteres)/Atividade_2.c
+++ Programação C/String (Array de caracteres)/Atividade_2.c
@@ -4,3 +4,98 @@
 // Por exemplo, se a string digitada for adoro programar em c a sa ́ıda deve ser zwlil kiltiznzi
 // vn x.
 // OBS. Considere apenas letras min ́usculas e desconsidere caracteres especiais (*, #, @, etc.).
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+
+    char str[30];
+    char letra_convertida;
+
+    printf("Digite uma frase: \n");
+    fgets(str, 30, stdin);
+
+    for(int i = 0; i < 30; i++){
+        if(str[i] == 'a'){
+            letra_convertida = 'z';
+            printf("%c", letra_convertida);
+        } else if(str[i] == 'b') {
+            letra_convertida = 'y';
+            printf("%c", letra_convertida);
+        } else if(str[i] == 'c') {
+            letra_convertida = 'x';
+            printf("%c", letra_convertida);
+        } else if(str[i] == 'd') {
+            letra_convertida = 'w';
+            printf("%c", letra_convertida);
+        } else if(str[i] == 'e') {
+            letra_convertida = 'v';
+            printf("%c", letra_convertida);
+        } else if(str[i] == 'f') {
+            letra_convertida = 'u';
+            printf("%c", letra_convertida);
+        } else if(str[i] == 'g') {
+            letra_convertida = 't';
+            printf("%c", letra_convertida);
+        } else if(str[i] == 'h') {
+            letra_convertida = 's';
+            printf("%c", letra_convertida);
+        } else if(str[i] == 'i') {
+            letra_convertida = 'r';
+            printf("%c", letra_convertida);
+        } else if(str[i] == 'j') {
+            letra_convertida = 'q';
+            printf("%c", letra_convertida);
+        } else if(str[i] == 'k') {
+            letra_convertida = 'p';
+            printf("%c", letra_convertida);
+        } else if(str[i] == 'l') {
+            letra_convertida = 'o';
+            printf("%c", letra_convertida);
+        } else if(str[i] == 'm') {
+            letra_convertida = 'n';
+            printf("%c", letra_convertida);
+        } else if(str[i] == 'n') {
+            letra_convertida = 'm';
+            printf("%c", letra_convertida);
+        } else if(str[i] == 'o') {
+            letra_convertida = 'l';
+            printf("%c", letra_convertida);
+        } else if(str[i] == 'p') {
+            letra_convertida = 'k';
+            printf("%c", letra_convertida);
+        } else if(str[i] == 'q') {
+            letra_convertida = 'j';
+            printf("%c", letra_convertida);
+        } else if(str[i] == 'r') {
+            letra_convertida = 'i';
+            printf("%c", letra_convertida);
+        } else if(str[i] == 's') {
+            letra_convertida = 'h';
+            printf("%c", letra_convertida);
+        } else if(str[i] == 't') {
+            letra_convertida = 'g';
+            printf("%c", letra_convertida);
+        } else if(str[i] == 'u') {
+            letra_convertida = 'f';
+            printf("%c", letra_convertida);
+        } else if(str[i] == 'v') {
+            letra_convertida = 'e';
+            printf("%c", letra_convertida);
+        } else if(str[i] == 'w') {
+            letra_convertida = 'd';
+            printf("%c", letra_convertida);
+        } else if(str[i] == 'x') {
+            letra_convertida = 'c';
+            printf("%c", letra_convertida);
+        } else if(str[i] == 'y') {
+            letra_convertida = 'b';
+            printf("%c", letra_convertida);
+        } else if (str[i] == 'z'){
+            letra_convertida = 'a';
+            printf("%c", letra_convertida);
+        }
+    }
+    return 0;
+}
